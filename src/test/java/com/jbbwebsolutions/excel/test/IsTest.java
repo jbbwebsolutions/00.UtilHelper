12df0070e28a2bb372b4070aa64621a33a18cd8a
jbbwebsolutions <jbbwebsolutions@gmail.com>
2018-08-17 09:43:55 -0400

    Added more test isFunction

--- src/test/java/com/jbbwebsolutions/excel/test/IsTest.java
+++ src/test/java/com/jbbwebsolutions/excel/test/IsTest.java
@@ -1,6 +1,7 @@
 package com.jbbwebsolutions.excel.test;
 
 import static com.jbbwebsolutions.excel._Num.isGreaterThan;
+import static com.jbbwebsolutions.excel._Num.isLessThan;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +11,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class IsTest {
 	
-	@DisplayName("isEqualToOrGreaterThan")
+	@DisplayName("isGreaterThan")
 	@ParameterizedTest(name = "\"{0}\" should be {1}")
 	@CsvSource({"true, 102, 100",
 				"true, 32.40002, 32.4",
@@ -20,6 +21,20 @@ class IsTest {
 	void isEqualToOrGreaterThan(boolean result, float value1, float value2) {	
 		
 		boolean status = isGreaterThan(value1, value2);		
+		assertTrue(status == result); 
+	}
+	
+	@DisplayName("isLessThan")
+	@ParameterizedTest(name = "\"{0}\" should be {1}")
+	@CsvSource({"true, 77, 100",
+				"true, 2.002, 32.4",
+				"true, 3.4, 132.4",
+				"false, 40, 40",
+				"true ,4.3, 32.5"})
+	
+	void isLessThanTest(boolean result, float value1, float value2) {
+		
+		boolean status = isLessThan(value1, value2);		
 		assertTrue(status == result); 
 	}
 	
