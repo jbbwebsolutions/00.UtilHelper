8cfe4860eece5dd41c0b77b82cbcf506e3589c16
jbbwebsolutions <jbbwebsolutions@gmail.com>
2018-08-19 14:35:58 -0400

    added comments

--- src/main/java/com/jbbwebsolutions/utils/Clazz.java
+++ src/main/java/com/jbbwebsolutions/utils/Clazz.java
@@ -4,6 +4,26 @@ import java.lang.reflect.*;
 
 public abstract class Clazz {
 	
+	/**
+	 * <p>The objective is to create execute a method
+	 * by knowing the name of the class and method names.
+	 * </p> 
+	 * 
+	 *  String className = "edu.citytech.cst.business.Foo";
+	 *  String method = "getNum";
+	 *  Class<?>[] parameterTypes = {String.class, Integer.TYPE};
+	 *  bject[] arguments =  {"all", 5}  ;
+	 *  String i = Clazz.execute(className, method, parameterTypes, arguments  );
+	 *  int i2 = Clazz.execute(className, method );
+	 *  System.out.println(i);
+	 *  System.out.println(i2);
+	 *  
+	 *  @author  jbbwebsolutions
+	 *  @see     n/a
+	 *  @since   JDK8.0		 
+	 * 
+	 */
+	
 	
 	public static <T> T execute(String className, String method) {	
 		Class<?>[] parameterTypes = {};		
@@ -24,17 +44,5 @@ public abstract class Clazz {
 		}
 
 		return t;
-	}
-
-	public static void main(String[] args) {
-		String className = "edu.citytech.cst.business.Foo";
-		String method = "getNum";
-		Class<?>[] parameterTypes = {String.class, Integer.TYPE};
-		
-		Object[] arguments =  {"all", 5}  ;
-		String i = Clazz.execute(className, method, parameterTypes, arguments  );
-		int i2 = Clazz.execute(className, method );
-		System.out.println(i);
-		System.out.println(i2);
-	}
+	}	
 }
